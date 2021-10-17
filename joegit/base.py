0904214f469cced64d489bad62357fdd0e4df1d1
joegurr <joseph.gurr@outlook.com>
2021-10-18 08:20:17 +1100

    Finished write-tree
    Now up to:
    
    https://www.leshenko.net/p/ugit/#read-tree

--- joegit/base.py
+++ joegit/base.py
@@ -4,13 +4,23 @@ from . import data
 
 
 def write_tree(directory="."):
+    entries = []
     with os.scandir(directory) as it:
         for entry in it:
             full = f"{directory}/{entry.name}"
+            if is_ignored(full):
+                continue
             if entry.is_file(follow_symlinks=False):
-                # TODO Write the file to object store
-                print(full)
+                type = "blob"
+                with open(full, "rb") as f:
+                    old = data.hash_object(f.read())
             elif entry.is_dir(follow_symlinks=False):
-                write_tree(full)
+                type_ = "tree"
+                old = write_tree(full)
+            entries.append((entry.name, old, type))
+    tree = "".join(f"{type_} {old} {name}\n" for name, old, type_ in sorted(entries))
+    return data.hash_object(tree.encode(), "tree")
 
-    # TODO actually create the tree object
+
+def is_ignored(path):
+    return ".joegit" in path.split("/")
